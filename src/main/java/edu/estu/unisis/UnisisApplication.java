8d81ac8267e8683af560aafe36077d1db19d8137
Muhammed Salih KOÇ <muhammed_salih_koc@ogr.eskisehir.edu.tr>
2025-06-27 10:38:43 +0300

    first commit

---
+++ src/main/java/edu/estu/unisis/UnisisApplication.java
@@ -0,0 +1,13 @@
+package edu.estu.unisis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnisisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/estu/unisis/UnisisApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.estu.unisis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
