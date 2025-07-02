d0232c4c1f98e462be1d9d15c862878c82920da0
Muhammed Salih KOÇ <muhammed_salih_koc@ogr.eskisehir.edu.tr>
2025-07-02 10:17:54 +0300

    Add a responsive, collapsible sidebar menu

--- src/main/resources/static/uploads/js/main.js
+++ src/main/resources/static/uploads/js/main.js
@@ -1,1 +1,17 @@
-
+window.addEventListener('DOMContentLoaded', function() {
+    const isAdmin = document.body.dataset.isAdmin === "true";
+    const isDesktop = window.innerWidth >= 992;
+    if(isAdmin && isDesktop) {
+        toggleNav();
+    }
+});
+function toggleNav() {
+  var sidebar = document.getElementById("mySidebar");
+  var main = document.getElementById("main");
+  sidebar.classList.toggle("show");
+  main.classList.toggle("shifted");
+}
+document.getElementById("closeSidebarBtn").onclick = function() {
+    document.getElementById("mySidebar").classList.remove("show");
+    document.getElementById("main").classList.remove("shifted");
+};
